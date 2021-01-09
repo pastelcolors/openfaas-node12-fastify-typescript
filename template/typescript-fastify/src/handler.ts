3ecb98574d76032cc4a67fe1768e2e6a5718b4a9
Adrian <itsadriandayrit@gmail.com>
2021-01-09 21:33:01 +0800

    Create handler.ts

---
+++ template/typescript-fastify/src/handler.ts
@@ -0,0 +1,9 @@
+import { ContextPayload, EventPayload } from './types';
+
+export default async (event: EventPayload, context: ContextPayload) => {
+  const result = {
+    message: 'Body: ' + JSON.stringify(event.body),
+  }
+
+  return context.code(200).send(result)
+}
